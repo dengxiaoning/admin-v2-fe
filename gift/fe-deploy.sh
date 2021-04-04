988475b6c4177a3a85d089eae9c68bb20f27eaec
dex <412693453@qq.com>
2021-04-04 15:37:02 +0800

    Add nginx config

---
+++ gift/fe-deploy.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+GIT_HOME=/opt/developer/git-repository/
+DEST_PATH=/opt/product/front/
+
+if [ ! -n "$1" ];
+then
+    echo -e "Please input a project name! You can input as follows:"
+    echo -e "./fe-deploy.sh admin-v2-fe"
+    exit
+fi
+
+if [ $1 = "admin-v2-fe" ];
+then
+    echo -e "----------------Enter Project---------------"
+    cd $GIT_HOME$1
+else
+    echo -e "Invalid Project Name!"
+    exit
+fi
+
+# clean dist 
+echo -e "----------------Clean Dist---------------"
+rm -rf ./dist
+
+echo -e "----------------Git  Pull---------------"
+git pull
+
+
+
+echo -e "----------------Npm Install --unsafe-perm=true --allow-root---------------"
+npm install --unsafe-perm=true --allow-root
+
+
+echo -e "----------------Npm run Dist---------------"
+npm run dist
+
+
+if [ -d "./dist" ];
+then
+    echo -e "----------------Clean Dist---------------"
+    rm -rf $DEST_PATH/dist
+
+    echo -e "----------------Copy Dist---------------"
+    cp -R ./dist $DEST_PATH/$1/
+
+
+    echo -e "----------------Deploy Success---------------"
+else
+    echo -e "----------------Deploy Fail---------------"
+fi
